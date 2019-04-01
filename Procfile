4a14b91a409595da51733694519f05b14b5fdc4f
José Medeiros <00archangel00@gmail.com>
2019-04-01 20:37:16 -0300

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sdmessenger.wsgi --log-file -
---
+++ messenger/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase, Client
+
+
+class ViewTest(TestCase):
+
+    def testView(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        response = c.get('/chat/')
+        self.assertEqual(response.status_code, 200)
+
+        response = c.get('/chat/lobby/')
+        self.assertEqual(response.status_code, 200)
